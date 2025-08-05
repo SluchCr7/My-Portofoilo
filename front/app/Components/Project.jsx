a655e42acf41b659bece950c150b880afde20766
Sluch <ahmedking10710@gmail.com>
2025-08-05 19:12:48 +0300

    new commit change v3

--- front/app/Components/Project.jsx
+++ front/app/Components/Project.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import React from 'react'
-import { techIcons } from '@/lib/techIcons';
+import { techIcons } from '@/lib/Data';
 const Project = ({project , setProject , idx}) => {
   return (
     <div
